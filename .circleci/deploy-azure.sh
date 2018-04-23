b4f9f9ee528276b2d1d474ac610fd6bba1a564b3
Jun Zheng <me@jackzh.com>
2018-04-23 02:03:37 -0400

    add debug line

--- .circleci/deploy-azure.sh
+++ .circleci/deploy-azure.sh
@@ -18,6 +18,9 @@ echo "Installing Azure components"
 # NOTE(bacongobbler): azure-cli needs a newer version of libffi/libssl. See https://github.com/Azure/azure-cli/issues/3720#issuecomment-350335381
 AZ_REPO=$(lsb_release -cs)
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ ${AZ_REPO} main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
+
+sudo cat /etc/apt/sources.list.d/azure-cli.list
+
 sudo apt-key adv --keyserver packages.microsoft.com --recv-keys 52E16F86FEE04B979B07E28DB02C46DF417A0893
 sudo apt-get install apt-transport-https
 sudo apt-get update && sudo apt-get install azure-cli
