4b6fdbf49eff6ded2cecdf91972d569ecd9919f6
Jun Zheng <me@jackzh.com>
2018-04-23 02:22:19 -0400

    Fix lsb_release

--- .circleci/deploy-azure.sh
+++ .circleci/deploy-azure.sh
@@ -16,8 +16,7 @@ fi
 
 echo "Installing Azure components"
 # NOTE(bacongobbler): azure-cli needs a newer version of libffi/libssl. See https://github.com/Azure/azure-cli/issues/3720#issuecomment-350335381
-AZ_REPO=$(lsb_release -cs)
-echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | \
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $(lsb_release -cs) main" | \
      sudo tee /etc/apt/sources.list.d/azure-cli.list
 sudo apt-key adv --keyserver packages.microsoft.com --recv-keys 52E16F86FEE04B979B07E28DB02C46DF417A0893
 curl -L https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
