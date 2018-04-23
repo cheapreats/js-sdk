c7638d277c8278555b02abf191439d29753e7fa1
Jun Zheng <me@jackzh.com>
2018-04-23 02:00:40 -0400

    Change pip to apt

--- .circleci/deploy-azure.sh
+++ .circleci/deploy-azure.sh
@@ -16,9 +16,12 @@ fi
 
 echo "Installing Azure components"
 # NOTE(bacongobbler): azure-cli needs a newer version of libffi/libssl. See https://github.com/Azure/azure-cli/issues/3720#issuecomment-350335381
-sudo apt-get update && sudo apt-get install -yq python3-pip libffi-dev libssl-dev
-sudo easy_install pyOpenSSL
-sudo pip3 install azure-cli~=2.0.31
+AZ_REPO=$(lsb_release -cs)
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | \
+     sudo tee /etc/apt/sources.list.d/azure-cli.list
+sudo apt-key adv --keyserver packages.microsoft.com --recv-keys 52E16F86FEE04B979B07E28DB02C46DF417A0893
+sudo apt-get install apt-transport-https
+sudo apt-get update && sudo apt-get install azure-cli
 
 echo "Pushing to Azure Blob Storage"
 sudo az storage blob upload-batch --source . --destination "${AZURE_CONTAINER}" --pattern *.tar.gz*
