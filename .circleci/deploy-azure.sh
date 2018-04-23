e9a90552441e583cf6d2d50009477ca6c0b2f743
Jun Zheng <me@jackzh.com>
2018-04-23 03:02:44 -0400

    Better code

--- .circleci/deploy-azure.sh
+++ .circleci/deploy-azure.sh
@@ -24,7 +24,8 @@ curl -L https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 sudo apt-get install apt-transport-https
 sudo apt-get update && sudo apt-get install azure-cli
 
-tar -czf "${CIRCLE_BRANCH}-${CIRCLE_BUILD_NUM}.tar.gz" --exclude="${CIRCLE_BRANCH}-${CIRCLE_BUILD_NUM}.tar.gz" .
+ARCHIVE_NAME="${CIRCLE_BRANCH}-${CIRCLE_BUILD_NUM}.tar.gz"
+tar -czf "${ARCHIVE_NAME}" --exclude="${ARCHIVE_NAME}" .
 
 echo "Pushing to Azure Blob Storage"
-az storage blob upload --file "${CIRCLE_BRANCH}-${CIRCLE_BUILD_NUM}.tar.gz" --container-name "${AZURE_CONTAINER}" --name "${CIRCLE_BRANCH}-${CIRCLE_BUILD_NUM}.tar.gz" --debug
+az storage blob upload --file "${ARCHIVE_NAME}" --container-name "${AZURE_CONTAINER}" --name "${ARCHIVE_NAME}" --debug
