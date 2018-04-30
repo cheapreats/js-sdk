430774007b1520450909e129fd3f1c228da148a4
Jun Zheng <me@jackzh.com>
2018-04-29 20:56:56 -0400

    Change QA endpoint for API

--- config/endpoints.js
+++ config/endpoints.js
@@ -1,11 +1,11 @@
 module.exports = {
     apolloEndpoint: {
         'production': 'https://utsc-food.azurewebsites.net/graphql',
-        'qa': 'https://api-test.cheapreats.com/graphql'
+        'qa': 'https://cheapreatsmainapi-test.azurewebsites.net/graphql'
     },
     verificationEndpoint: {
         'production': 'https://utsc-food.azurewebsites.net/get_code',
-        'qa': 'https://api-test.cheapreats.com/get_code'
+        'qa': 'https://cheapreatsmainapi-test.azurewebsites.net/get_code'
     },
     notificationEndpoint: {
         'production': 'https://func-notification-services-qa.cheapreats.com',
