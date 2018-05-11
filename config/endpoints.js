5d2d662eb59a7ec5f205531153c4414a2c63a0ae
Jun Zheng <me@jackzh.com>
2018-05-11 14:05:13 -0400

    Remove test, change production API to beta one.

--- config/endpoints.js
+++ config/endpoints.js
@@ -1,14 +1,14 @@
 module.exports = {
     apolloEndpoint: {
-        'production': 'https://utsc-food.azurewebsites.net/graphql',
+        'production': 'https://cheapreatsbetaroundoneapi.azurewebsites.net/graphql',
         'qa': 'https://cheapreatsmainapi-test.azurewebsites.net/graphql'
     },
     restEndpoint: {
-        'production': 'https://utsc-food.azurewebsites.net',
+        'production': 'https://cheapreatsbetaroundoneapi.azurewebsites.net',
         'qa': 'https://cheapreatsmainapi-test.azurewebsites.net'
     },
     notificationEndpoint: {
-        'production': 'https://func-notification-services-qa.cheapreats.com',
+        'production': 'https://cheapreatsnotificationservicesbetaroundone.azurewebsites.net',
         'qa': 'https://func-notification-services-qa.cheapreats.com'
     }
 };
