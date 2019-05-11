7389acd363136f71c178abda258b2278412d1c9b
bholagabbar <dev.shreyans96@gmail.com>
2019-05-11 13:36:22 +0530

    Updating createFlasSale params

--- src/app/controllers/FlashSaleController.js
+++ src/app/controllers/FlashSaleController.js
@@ -2,8 +2,8 @@
  * Controller related to flash sales
  */
 class FlashSaleController {
-    constructor(app){
-        this.app = app;
+    constructor(app) {
+        this.app    = app;
         // ADD BINDINGS BELOW
         this.create = this.create.bind(this);
         this.update = this.update.bind(this);
@@ -14,22 +14,24 @@ class FlashSaleController {
     /**
      * Create a new flash sale
      * @param {String} vendor_id - Vendor ID
+     * @param {String} type - If the flash sale is on PERCENTAGE or DOLLAR basis
+     * @param {int} amount - Amount in cents to base the flash sale off of
      * @param {Object[]} items - List of items included in Flash Sale
      * @param {String} start_at - Start time for Flash Sale in ISO format
      * @param {String} end_at - End time for Flash Sale in ISO format
      * @returns {Promise<any>}
      */
-    create(vendor_id, items, start_at, end_at){
+    create(vendor_id, type, amount, items, start_at, end_at) {
         return new Promise((resolve, reject) => {
             let mutationString = `
-                mutation($vendor_id:String!, $items: [FlashSaleItemInput]!, $start_at:String!, $end_at:String!) {
-                    createFlashSale(vendor_id: $vendor_id, items:$items, start_at:$start_at, end_at:$end_at) {
+                mutation($vendor_id: String!, type: FlashSaleType!, amount: Int!, $items: [FlashSaleItemInput]!, $start_at: String!, $end_at: String!) {
+                    createFlashSale(vendor_id: $vendor_id, type: $type, amount: $amount, items:$items, start_at:$start_at, end_at:$end_at) {
                         _id
                     }
                 }
             `;
             this.app.getAdaptor().mutate(mutationString, {
-                vendor_id, items, start_at, end_at
+                vendor_id, type, amount, items, start_at, end_at
             }).then(result => {
                 resolve(result.createFlashSale._id);
             }).catch(e => {
@@ -45,10 +47,10 @@ class FlashSaleController {
      * @param {String} end_at - End time for Flash Sale in ISO format
      * @returns {Promise<any>}
      */
-    update(id, items, end_at){
+    update(id, items, end_at) {
         return new Promise((resolve, reject) => {
             let mutationString = `
-                mutation($id:String!, $items: [FlashSaleItemInput], $end_at:String) {
+                mutation($id: String!, $items: [FlashSaleItemInput], $end_at: String) {
                     updateFlashSale(id: $id, items:$items, end_at:$end_at) {
                         _id
                     }
