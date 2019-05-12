04987e7fbdc00c9f3ff348af4260aaccc4bcb37b
Jun Zheng <me@jackzh.com>
2019-05-11 23:41:54 -0400

    fix: Add numbers to strToIdentifier

--- src/app/util/strToIdentifier.js
+++ src/app/util/strToIdentifier.js
@@ -1,7 +1,7 @@
 module.exports = function(str){
     let result = "";
     for(let i = 0; i < str.length; i++){
-        if(str[i].match(/[A-Za-z]/)){
+        if(str[i].match(/[A-Za-z0-9]/)){
             result += str[i].toLowerCase();
         } else if ((str[i] === '_' || str[i] === ' ' || str[i] === '-') && result[result.length - 1] !== '_'){
             result += '_';
