ff32af83983dedda0f06751bc6c39d89fb1048f7
Shreyans Sheth <dev.shreyans96@gmail.com>
2019-10-10 16:00:02 -0400

    CHEAP-996 fix: Add AuthorizationController having method for token scope (#30)
    * fix: Add AuthorizationController having method for token scope
    
    * Update src/app/controllers/AuthorizationController.js
    
    Co-Authored-By: Ralph Maamari <ralph.maamari@mail.utoronto.ca>
    
    * fix: spacing

--- src/app/App.js
+++ src/app/App.js
@@ -1,8 +1,9 @@
 const strToIdentifier          = require('./util/strToIdentifier');
 const CheaprEatsGraphQLAdaptor = require('./adaptors/CheaprEatsGraphQLAdaptor');
-const packageDotJson             = require('../../package.json');
+const packageDotJson           = require('../../package.json');
 
 // ADD CONTROLLER IMPORTS BELOW
+const AuthorizationController  = require('./controllers/AuthorizationController');
 const CartController           = require('./controllers/CartController');
 const CategoryController       = require('./controllers/CategoryController');
 const CustomerController       = require('./controllers/CustomerController');
@@ -49,6 +50,7 @@ class App {
             graphQLEndpoint: this.getConfiguration().endpoints.graphQLEndpoint.production
         });
         // ADD CONTROLLERS BELOW
+        this._authorizationController  = new AuthorizationController(this);
         this._cartController           = new CartController(this);
         this._categoryController       = new CategoryController(this);
         this._customerController       = new CustomerController(this);
@@ -76,6 +78,12 @@ class App {
     }
 
     // ADD GETTERS BELOW
+
+    get Authorization() {
+        return {
+            getTokenScope: this._authorizationController.getTokenScope,
+        };
+    }
 
     get Cart() {
         return {
