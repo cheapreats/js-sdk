ed4c1d57a788df168532b7d46debfc504117eb42
bholagabbar <dev.shreyans96@gmail.com>
2019-10-10 13:26:36 -0400

    fix: Add AuthorizationController having method for token scope

---
+++ src/app/controllers/AuthorizationController.js
@@ -0,0 +1,30 @@
+class AuthorizationController {
+    constructor(app) {
+        this.app           = app;
+        this.getTokenScope = this.getTokenScope.bind(this);
+    }
+
+    /**
+     * Get the authorization scope (role) for a user in the system through the issued token
+     * @param {string} token - Authorization Token Issued
+     * @returns {Promise<any>} - Role of the user in the system
+     */
+    getTokenScope(token) {
+        let queryString = `
+            query {
+                auth_token_scope(token:"${token}")
+            }
+        `;
+        return new Promise((resolve, reject) => {
+            return this.app.getAdaptor().query(queryString).then(data => {
+                resolve(data.auth_token_scope);
+            }).catch(e => {
+                reject(e);
+            });
+        });
+
+    }
+
+}
+
+module.exports = AuthorizationController;
