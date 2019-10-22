69f3afa99464cdc501cc08af4421ddb8c4889131
bholagabbar <dev.shreyans96@gmail.com>
2019-10-22 16:56:02 -0400

    update jsdoc, fix broken utils method

--- src/app/util/formatPrice.js
+++ src/app/util/formatPrice.js
@@ -1,13 +1,11 @@
-// @flow
-
 /**
  * Takes in a number and formats it with consistent price formatting.
  * @param {number} price
- * @param {boolean} showZero - Whether to show $0.00
- * @returns {string} Processed string.
+ * @param {boolean} showZero
+ * @returns {string}
  */
-export function formatPrice(price: number, showZero = true): string {
-  if (price !== 0 || showZero) {
-    return "$" + (price / 100).toFixed(2);
-  }
+export function formatPrice(price, showZero = true) {
+    if (price !== 0 || showZero) {
+        return "$" + (price / 100).toFixed(2);
+    }
 }
